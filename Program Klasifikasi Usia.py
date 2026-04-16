c6521f02efb99a7a2d80762bb23ffe55a45d0c27
SYAIFULRIDHO159 <syaifulridho159@gmail.com>
2026-04-17 01:16:12 +0800

    Refactor code using clean code principles

--- Program Klasifikasi Usia.py
+++ Program Klasifikasi Usia.py
@@ -1,10 +1,27 @@
-usia = int(input("berapakah usia anda?"))
+def input_usia():
+    while True:
+        try:
+            usia = int(input("Berapakah usia anda? "))
+            return usia
+        except ValueError:
+            print("Masukkan angka yang valid!")
 
-if usia <= 10:
-    print("anda masih anak-anak")
-elif usia <= 19:
-    print("anda remaja")
-elif usia <= 50:
-    print("anda dewasa")
-else:
-    print("anda lansia")
+
+def klasifikasi_usia(usia):
+    if usia <= 10:
+        return "Anda masih anak-anak"
+    elif usia <= 19:
+        return "Anda remaja"
+    elif usia <= 50:
+        return "Anda dewasa"
+    else:
+        return "Anda lansia"
+
+
+def main():
+    usia = input_usia()
+    hasil = klasifikasi_usia(usia)
+    print(hasil)
+
+
+main()
