870c7385b94df02b2485989afceaff143864e3d7
SYAIFULRIDHO159 <syaifulridho159@gmail.com>
2026-04-17 01:14:43 +0800

    Initial commit - Program klasifikasi usia sederhana

---
+++ Program Klasifikasi Usia.py
@@ -0,0 +1,10 @@
+usia = int(input("berapakah usia anda?"))
+
+if usia <= 10:
+    print("anda masih anak-anak")
+elif usia <= 19:
+    print("anda remaja")
+elif usia <= 50:
+    print("anda dewasa")
+else:
+    print("anda lansia")
